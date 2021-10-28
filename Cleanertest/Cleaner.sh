42605a9c11f47899c42bb4f3280e658dc6a45dda
Christoffer Eriksson <ch8820er-s@student.lu.se>
2021-10-28 05:11:12 +0200

    Added extra features into cleaner

--- Cleanertest/Cleaner.sh
+++ Cleanertest/Cleaner.sh
@@ -3,6 +3,20 @@ sed -e '1,12d' DATA > Cleaneddata
 sed -i 's/;/ /g' Cleaneddata 
 sed -i 's/Y.*//' Cleaneddata
 sed -i 's/G.*//' Cleaneddata
+sed -i 's/:/ /g' Cleaneddata 
+sed -i 's/  */ /g' Cleaneddata
+sed -i 's/^ //' Cleaneddata
+
+i=20
+x=12
+while [ $i -ge $x ]
+do
+	sed -i "s/.//$i" Cleaneddata
+	i=$(($i - 1));
+done
+
+
+
 
 
 
