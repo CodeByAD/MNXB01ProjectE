aee7df8bb71ee2c7639c459e566bd7f73ec4902d
Marcus Glad <gladmacke@hotmail.se>
2021-11-10 18:54:39 +0100

    Added callable functions for each result in project.cpp

--- Code/Skeleton/project.cpp
+++ Code/Skeleton/project.cpp
@@ -1,18 +1,32 @@
 #include "tempTrender.h"
 #include <string>
-void Cleaner(){
+void Cleaner() //Runs the cleaners for each result.
+{
 	system("Cleaner31.sh");
 	system("Cleaner33.sh");
 	system("Cleaner3D.sh");
 }
 
-void project() {
+void temponday() //Calls the function temponday from skeleton.
+{
 	
 	tempTrender t; //Instantiate your analysis object
-	//One must currently comment out the results one does not want to produce since only one can be done at once.
-	//t.tempOnDay(8, 23); //Result3.1.1
-	//t.tempOnDay(235); //Result 3.1.2
-	//t.extremetemps("2020"); //Result 3.3 Call with a single year to investigate
-				//Also plots for all available years in the dataset
-	t.Avgtemp3D();//3D average plot
+
+	t.tempOnDay(8, 23); //Result3.1.1
+	t.tempOnDay(235); //Result 3.1.2
 }
+
+void extremetemps() //Calls the function extremetemps from skeleton.
+{
+	tempTrender e;
+	e.extremetemps("2020");
+}
+
+void Avgtemp3D() //Calls the function Avgtemp3D from skeleton.
+{
+	tempTrender d;
+	d.Avgtemp3D();
+}
+
+
+
